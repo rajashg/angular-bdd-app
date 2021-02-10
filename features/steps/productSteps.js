0ba4a14edc47690b78201237a7b8e82eb091a620
rgore <rajashrigore@yahoo.com>
2021-02-10 14:04:26 -0600

    Fixed tests

--- features/steps/productSteps.js
+++ features/steps/productSteps.js
@@ -2,7 +2,7 @@ const { When, Then, After } = require('cucumber');
 const assert = require('assert');
 const { Builder, By, until } = require('selenium-webdriver');
  
-When('we request the products list', async function () {
+When('we request the products list', {timeout: 1 * 15000}, async function () {
     this.driver = new Builder()
         .forBrowser('chrome')
         .build();
@@ -12,19 +12,18 @@ When('we request the products list', async function () {
     await this.driver.get('http://localhost:4200');
 });
 
-Then('we should receive', async function (dataTable) {
+Then('we should receive', {timeout: 1 * 15000}, async function (dataTable) {
     var productElements = await this.driver.findElements(By.className('product'));
     var expectations = dataTable.hashes();
     for (let i = 0; i < expectations.length; i++) {
         const productName = await productElements[i].findElement(By.tagName('h3')).getText();
-        assert.equal(productName, expectations[i].name);
+        assert.equal(productName, expectations[i].Name);
 
         const description = await productElements[i].findElement(By.tagName('p')).getText();
-        assert.equal(description, `Description: ${expectations[i].description}`);
+        assert.equal(description, `Description: ${expectations[i].Description}`);
     }
 });
-/*
+
 After(async function() {
     this.driver.close();
 });
-*/
